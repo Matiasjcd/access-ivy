c7d5f6fb05a648a5f9383203f6ad38f6b5dee1d7
Matiasjcd <matiascaceresd@gmail.com>
2025-05-22 10:40:26 -0400

    Simplify things

--- app.py
+++ app.py
@@ -1,27 +1,6 @@
 from flask import Flask, render_template, send_from_directory
-import requests
-import threading
-import time
 
 app = Flask(__name__)
-
-
-# Periodic GET requests
-def periodic_get_request(url, interval=870):
-    return
-    while True:
-        try:
-            response = requests.get(url)
-            print(f"Request to {url} completed with status code: {response.status_code}")
-        except requests.exceptions.RequestException as e:
-            print(f"Error with request: {e}")
-        time.sleep(interval)
-
-# Start periodic GET requests in a separate thread
-url_to_request = "https://accessivytutoring.com/background-task"
-thread = threading.Thread(target=periodic_get_request, args=(url_to_request,))
-thread.daemon = True
-thread.start()
 
 
 @app.route('/')
